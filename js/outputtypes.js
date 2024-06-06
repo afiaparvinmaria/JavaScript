2c85a43ca4d903f3059ea39ea9d4af3bc8fefe9d
mariamsafa <mariamsafa153@gmail.com>
2024-06-06 21:30:19 +0600

    output input basic

---
+++ js/outputtypes.js
@@ -0,0 +1,15 @@
+ //output types
+       
+window.alert("Hello world!");
+//alert("Hello world!"); another way
+
+document.write("I am Safa");
+document.write(67);
+document.getElementById("root").innerHTML=" JavaScript";
+
+document.getElementById("idh1").innerHTML="H1ID";
+document.getElementById("idh2").innerHTML="H2ID";
+
+console.log("I am Bangali"); //built in console in a browser(chrome) which is the V8 engine 
+
+
---
+++ js/userinput.js
@@ -0,0 +1,4 @@
+var x;// var means a variable
+x=prompt("Enter your name: ");//taking input
+console.log(x);
+document.write(x);//printing x's value
