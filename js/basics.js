13651a60d9754398d94d91de924bd1ebabe92e28
mariamsafa <mariamsafa153@gmail.com>
2024-06-07 23:28:17 +0600

    var let

---
+++ js/basics.js
@@ -0,0 +1,13 @@
+var a;
+let b;
+a= 40;
+b="i am safa";
+console.log(a);
+console.log(b);
+
+const c=9; //constant variable
+console.log(b, c); //i am safa 9
+
+
+
+
--- js/userinput.js
+++ js/userinput.js
@@ -2,3 +2,5 @@ var x;// var means a variable
 x=prompt("Enter your name: ");//taking input
 console.log(x);
 document.write(x);//printing x's value
+var myname= 65;
+ 
