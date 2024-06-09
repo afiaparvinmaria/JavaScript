bf84ba37c7799dd134a77d78be8ee1ddd2fadd45
mariamsafa <mariamsafa153@gmail.com>
2024-06-10 01:12:01 +0600

    operators

--- js/basics.js
+++ js/basics.js
@@ -1,13 +1,34 @@
-var a;
-let b;
-a= 40;
-b="i am safa";
-console.log(a);
-console.log(b);
+// var a;
+// let b;//declaring a variable
+// a= 40;
+// b="i am safa";
+// console.log(a);
+// console.log(b);
+// document.write(a);
 
-const c=9; //constant variable
-console.log(b, c); //i am safa 9
+// const c=9; //constant variable
+// console.log(b, c); //i am safa 9
 
+// var l1="58";
+// var l2="Safa";
+// console.log(l1-l2)//NaN means not a number
+
+// //comparison operator
+// var s1= 4;
+// var s2="4";
+// var r1= s1==s2; //returns true //do not consider the type
+// r1= s1===s2;//returns false // considers the type
+
+// //conditional operator
+// var s3= (1<8) ? "Hello" : "world";//retruns Hello cz the condition is true
+
+
+
+
+var x= prompt("Temperature in Celcius");
+var f= 9/5*x + 32;
+alert("Farenheit: "+ f +" Degree");
+console.log("Task Complete!");
 
 
 
