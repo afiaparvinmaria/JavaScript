20cc42fd8bdf48d8dd507bde112c3bc9abf83b30
mariamsafa <mariamsafa153@gmail.com>
2024-09-13 23:25:37 +0600

    removed comment

--- js/functions.js
+++ js/functions.js
@@ -1,69 +1,69 @@
-// //built in functions
-// //- alert()
-// //- console.log()
+//built in functions
+//- alert()
+//- console.log()
 
 
-// function myfunc()
-// {
-//     console.log("i am safa")
+function myfunc()
+{
+    console.log("i am safa")
 
-// }
+}
 
-// myfunc()
-
-
-
-// //function paremeter/argument
-
-// function func(a,b, fname="fn", lname="ln")//parameter
-// //if there is no parameter fname or lname recieve then it will print fn ln by default
-
-// {
-//     console.log(`Hello ${ fname } ${ lname }!`);
-//     var a,b
-//     var sum= a+b
-//     console.log(sum)
-
-// }
-// func(5,8,"mariam","safa")//arguments
+myfunc()
 
 
 
-// function addnum(a=0,b=0){
-//     console.log(a+b)
-// }
-// addnum(4,5)
-// addnum(2.5,2.5)
+//function paremeter/argument
+
+function func(a,b, fname="fn", lname="ln")//parameter
+//if there is no parameter fname or lname recieve then it will print fn ln by default
+
+{
+    console.log(`Hello ${ fname } ${ lname }!`);
+    var a,b
+    var sum= a+b
+    console.log(sum)
+
+}
+func(5,8,"mariam","safa")//arguments
 
 
 
-// //functional expression
-// let func1= function(name){
-//     console.log('Hello '+name)
-
-// }
-
-
-// //Arrow Function (ES6) Before ES6, functions in JavaScript were defined using the function keyword. 
-
-// let func2=(name2)=>{
-//     console.log("Hello "+ name2)
-
-// }
-// func1("Humayra")
-// func2("simanta")
+function addnum(a=0,b=0){
+    console.log(a+b)
+}
+addnum(4,5)
+addnum(2.5,2.5)
 
 
 
-// Array iteration using forEach
-// var foods = ["Cake", "Ice Cream", "Chocolates", "Bread"];
-// var numbers = [1, 32, 31, 2];
-// let printeverything = function(item,i,abc){
-//     console.log(`Index : ${i} and Item: ${item}`)
-//     console.log(abc)
-// }
-// //array.forEach(callback)
-// foods.forEach(printeverything)//using foreach loop in foods array
+//functional expression
+let func1= function(name){
+    console.log('Hello '+name)
+
+}
+
+
+//Arrow Function (ES6) Before ES6, functions in JavaScript were defined using the function keyword. 
+
+let func2=(name2)=>{
+    console.log("Hello "+ name2)
+
+}
+func1("Humayra")
+func2("simanta")
+
+
+
+//Array iteration using forEach
+var foods = ["Cake", "Ice Cream", "Chocolates", "Bread"];
+var numbers = [1, 32, 31, 2];
+let printeverything = function(item,i,abc){
+    console.log(`Index : ${i} and Item: ${item}`)
+    console.log(abc)
+}
+//array.forEach(callback)
+foods.forEach(printeverything)//using foreach loop in foods array
 
 /* When you call array.forEach(callback), JavaScript processes the array in a very specific way. The forEach() method is built into JavaScript and knows that:
 
@@ -73,33 +73,17 @@ Third Argument (array): It always passes the entire array being iterated over as
 
 
 
-<<<<<<< HEAD
 // Array iteration using Mapping
 /* var food1 = ["Cake", "Ice Cream", "Chocolates", "Bread"];
 var number = [1, 32, 31, 2];
 function addsomething(item){
     return `${item} is a food`
 }
-=======
-//// Array iteration using Mapping
-var foods = ["Cake", "Ice Cream", "Chocolates", "Bread"];
-var numbers = [1, 32, 31, 2];
->>>>>>> dabfc1e49652e593c90356f003ca3823c69e6f21
 
-
-function addSomething(item) {
-    return `${item} is a Food`;
-}
-/*function squarenumbers(item){
+let show= food1.map(addsomething)
+let show1= number.map(function(item){
     return item*item
-}*/
-let arr_res = foods.map(addSomething);
-//Here, the map function applies addSomething to each element of the foods array, producing a new array arr_res where each item is a string indicating that the food item is indeed a food.
-let arr_square = numbers.map(function(item){ //annonymoous function declaration
-    return item*item;
-    //let arr_square = numbers.map(squarenumbers);
 })
-<<<<<<< HEAD
 console.log(show)
 console.log(show1) */
 
@@ -119,16 +103,3 @@ let person = {
 }
 console.log(person.firstname);
 console.log(person.fullname());
-=======
-
-console.log(arr_res);
-console.log(arr_square); 
-/*output:[
-  'Cake is a Food',
-  'Ice Cream is a Food',
-  'Chocolates is a Food',
-  'Bread is a Food'
-]
-[ 1, 1024, 961, 4 ]
-*/
->>>>>>> dabfc1e49652e593c90356f003ca3823c69e6f21
